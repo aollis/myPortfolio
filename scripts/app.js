c66870a729e6031264442995180f6233e9a3e544
aaronollis <aaronollis121@gmail.com>
2015-06-28 12:06:42 -0700

    fixed bootstrap issue, made code more modular as result

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-//script goes here //
+
+		$("#homeButton").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#navBar").offset().top},
+        	1300);
+		});
+		
+		$("#aboutButton").click(function() {
+		$('html, body').animate({
+        	scrollTop: $("#about").offset().top},
+        	1300);
+		});
+
+		$("#projectsButton").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#projects").offset().top},
+        	1300);
+		});
+
+		$("#contactButton").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#footer").offset().top},
+        	1300);
+		});

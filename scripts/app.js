87fc2ccf1dfc9f916614f111cc9d5fe171dd9f00
aaronollis <aaronollis121@gmail.com>
2015-06-29 11:14:49 -0700

    structure changes, more functionality and color choices to come

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,24 @@
 
-		$("#homeButton").click(function() {
-    	$('html, body').animate({
-        	scrollTop: $("#navBar").offset().top},
-        	1300);
-		});
+$("#homeButton").click(function() {
+	$('html, body').animate({
+        scrollTop: $("#navBar").offset().top},
+        1300);
+});
 		
-		$("#aboutButton").click(function() {
-		$('html, body').animate({
-        	scrollTop: $("#about").offset().top},
-        	1300);
-		});
+$("#aboutButton").click(function() {
+	$('html, body').animate({
+       	scrollTop: $("#about").offset().top},
+       	1300);
+});
 
-		$("#projectsButton").click(function() {
-    	$('html, body').animate({
-        	scrollTop: $("#projects").offset().top},
-        	1300);
-		});
+$("#projectsButton").click(function() {
+    $('html, body').animate({
+       	scrollTop: $("#projects").offset().top},
+       	1300);
+});
 
-		$("#contactButton").click(function() {
-    	$('html, body').animate({
-        	scrollTop: $("#footer").offset().top},
-        	1300);
-		});
+$("#contactButton").click(function() {
+    $('html, body').animate({
+    	scrollTop: $("#footer").offset().top},
+       	1300);
+});
